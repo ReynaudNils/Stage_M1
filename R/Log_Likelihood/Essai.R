76f949a465c49dedb665d86f1a6dce81c86694cf
Nils Reynaud <nils.reynaud@ensta-paris.fr>
2022-07-29 14:13:53 +0200

    Modélisation Mandelbrot
    Calcul liés au modèle Zipf-Mandelbrot (max de vraisemblance, gradient, hessienne) et poursuite de la modélisation de Genre Sedum sous R avec ce modèle Zipf-Mandelbrot, utilisation des différents outils optim, nlm, mle etc mais sans réussite: le gradient encodé ne semble pas le bon, il n'est pas nul aux points optimaux

---
+++ R/Log_Likelihood/Essai.R
@@ -0,0 +1,31 @@
+#set.seed(123)
+
+## set a N = 10
+#N <- 10
+
+## populate the negative log likelihood function
+#negloglik <- function(parms, x=x) {
+#  H <- sum(1/(1:N + parms[1])^parms[2])
+#  -sum(log(1/(x + parms[1])^parms[2]/H))
+#}
+
+## generate pmf for sampling
+#plot(d <- sapply(1:10, function(x) exp(-negloglik(c(5,5), x))))
+
+## draw a random sample with n=1000
+#x <- sample(1:10, 1000, replace = T, prob = d)
+#x
+## calculate ml estimates
+#ml <- nlm(function(p) negloglik(p, x=x), p=c(20,20))
+
+#getwd()
+#setwd("C:/Users/nilsr")
+#N <- 59    # the size of my dataset
+#x <- read.csv("Especes_Sedum.csv")    # my dataset file
+#negloglik <- function(parms, x=x) {
+#       H <- sum(1/(1:N + parms[1])^parms[2])
+#       -sum(log(1/(x + parms[1])^parms[2]/H))
+#       }
+#plot(d <- sapply(1:N, function(x) exp(-negloglik(c(5,5), x))))  # here I changed 10 to N (the size of my data)
+#x <- sample(1:N, 1000, replace = T, prob = d)
+#ml <- nlm(function(p) negloglik(p, x=x), p=c(20,20))
--- R/Log_Likelihood/Mandelbrot_Modelisation.R
+++ R/Log_Likelihood/Mandelbrot_Modelisation.R
@@ -1,41 +1,14 @@
-#set.seed(123)
-
-## set a N = 10
-#N <- 10
-
-## populate the negative log likelihood function
-#negloglik <- function(parms, x=x) {
-#  H <- sum(1/(1:N + parms[1])^parms[2])
-#  -sum(log(1/(x + parms[1])^parms[2]/H))
-#}
-
-## generate pmf for sampling
-#plot(d <- sapply(1:10, function(x) exp(-negloglik(c(5,5), x))))
-
-## draw a random sample with n=1000
-#x <- sample(1:10, 1000, replace = T, prob = d)
-#x
-## calculate ml estimates
-#ml <- nlm(function(p) negloglik(p, x=x), p=c(20,20))
-
-#getwd()
-#setwd("C:/Users/nilsr")
-#N <- 59    # the size of my dataset
-#x <- read.csv("Especes_Sedum.csv")    # my dataset file
-#negloglik <- function(parms, x=x) {
-#       H <- sum(1/(1:N + parms[1])^parms[2])
-#       -sum(log(1/(x + parms[1])^parms[2]/H))
-#       }
-#plot(d <- sapply(1:N, function(x) exp(-negloglik(c(5,5), x))))  # here I changed 10 to N (the size of my data)
-#x <- sample(1:N, 1000, replace = T, prob = d)
-#ml <- nlm(function(p) negloglik(p, x=x), p=c(20,20))
-
+getwd()
 setwd('C:/Users/nilsr/Desktop/PRE/Stage_M1/Donnees_CSV')
+library(stats4)
 
 N <- 59    # the size of my dataset
 Genera_Sedum <- read.csv("Especes_Sedum.csv")    # my dataset file
 
 x=Genera_Sedum$Images_Sedum
+
+#Définissons la fonction maximum de vraisemblance
+#D'abord juste le negloglikelihood
 negloglik <- function(parms) {
   invC <- sum(1/(1:N + parms[1])^parms[2])
   return(-sum(log(1/((x + parms[1])^(parms[2])*invC))))
@@ -44,50 +17,31 @@ negloglik2 <- function(parms1, parms2) {
   invC <- sum(1/(1:N + parms1)^parms2)
   return(-sum(log(1/((x + parms1)^(parms2)*invC))))
 }
+
+#Ensuite avec le gradient en plus
 negloglik3 <- function(parms) {
   invC <- sum(1/(1:N + parms[1])^parms[2])
   res <- -sum(log(1/((x + parms[1])^(parms[2])*invC)))
-  A <- sum(parms[2]/x + parms[1])
-  B <- sum(log(x + parms[1]))
+  tampon <- sum(-parms[2]/(1:N + parms[1])^(parms[2]+1))
+  tampon2 <- sum(exp(-parms[2]*log(1:N + parms[1])))
+  tampon3 <- sum(-log(1:N + parms[1])*exp(-parms[2]*log(1:N + parms[1])))
+  A <- sum((parms[2]/(x + parms[1]))) + (tampon/invC)
+  B <- sum(log(x + parms[1])) + (tampon3/tampon2)
   attr(res, "gradient") <- c(A, B)
   res
 }
 negloglik4 <- function(parms1, parms2) {
   invC <- sum(1/(1:N + parms1)^parms2)
   res <- -sum(log(1/((x + parms1)^(parms2)*invC)))
-  A <- sum(parms2/x + parms1)
-  B <- sum(log(x + parms1))
-  attr(res, "gradient") <- c(A, B)
+  tampon <- sum(-parms2/(1:N + parms1)^(parms2+1))
+  tampon2 <- sum(exp(-parms2*log(1:N + parms1)))
+  tampon3 <- sum(-log(1:N + parms1)*exp(-parms2*log(1:N + parms1)))
+  A <- sum((parms2/(x + parms1))) + (tampon/invC)
+  B <- sum(log(x + parms1)) + (tampon3/tampon2)
   res
 }
-negloglik5 <- function(parms) {
-  invC <- sum(1/(1:N + parms[1])^parms[2])
-  res <- -sum(log(1/((x + parms[1])^(parms[2])*invC)))
-  A <- sum(parms[2]/x + parms[1])
-  B <- sum(log(x + parms[1]))
-  H <- matrix(1:4, nrow=2, byrow=TRUE)
-  H[1,1] <- -sum(parms[2]/(x + parms[1])^2)
-  H[2,2] <- 0
-  H[1,2] <- sum(1/(x + parms[1]))
-  H[2,1] <- sum(1/(x + parms[1]))
-  attr(res, "gradient") <- c(A, B)
-  attr(res, "hessian") <- H
-  res
-}
-negloglik6 <- function(parms1, parms2) {
-  invC <- sum(1/(1:N + parms1)^parms2)
-  res <- -sum(log(1/((x + parms1)^(parms2)*invC)))
-  A <- sum(parms2/x + parms1)
-  B <- sum(log(x + parms1))
-  H <- matrix(1:4, nrow=2, byrow=TRUE)
-  H[1,1] <- -sum(parms2/(x + parms1)^2)
-  H[2,2] <- 0
-  H[1,2] <- sum(1/(x + parms1))
-  H[2,1] <- sum(1/(x + parms1))
-  attr(res, "gradient") <- c(A, B)
-  attr(res, "hessian") <- H
-  res
-}
+
+#La fonction gradient
 gradient <- function(parms) {
   invC <- sum(1/(1:N + parms[1])^parms[2])
   tampon <- sum(-parms[2]/(1:N + parms[1])^(parms[2]+1))
@@ -97,51 +51,94 @@ gradient <- function(parms) {
   B <- sum(log(x + parms[1])) + (tampon3/tampon2)
   return(c(A, B))
 }
-gradient(optim2$par)
-library(stats4)
+
+#Enfin avec la hessienne en plus, à coder avec les bons termes
+#negloglik5 <- function(parms) {
+#  invC <- sum(1/(1:N + parms[1])^parms[2])
+#  res <- -sum(log(1/((x + parms[1])^(parms[2])*invC)))
+#  tampon <- sum(-parms[2]/(1:N + parms[1])^(parms[2]+1))
+#  tampon2 <- sum(exp(-parms[2]*log(1:N + parms[1])))
+#  tampon3 <- sum(-log(1:N + parms[1])*exp(-parms[2]*log(1:N + parms[1])))
+#  A <- sum((parms[2]/(x + parms[1]))) + (tampon/invC)
+#  B <- sum(log(x + parms[1])) + (tampon3/tampon2)
+#  H <- matrix(1:4, nrow=2, byrow=TRUE)
+#  H[1,1] <- -sum(parms[2]/(x + parms[1])^2)
+#  H[2,2] <- 0
+#  H[1,2] <- sum(1/(x + parms[1]))
+#  H[2,1] <- sum(1/(x + parms[1]))
+#  attr(res, "gradient") <- c(A, B)
+#  attr(res, "hessian") <- H
+#  res
+#}
+#negloglik6 <- function(parms1, parms2) {
+#  invC <- sum(1/(1:N + parms1)^parms2)
+#  res <- -sum(log(1/((x + parms1)^(parms2)*invC)))
+#  invC <- sum(1/(1:N + parms1)^parms2)
+#  tampon <- sum(-parms2/(1:N + parms1)^(parms2+1))
+#  tampon2 <- sum(exp(-parms2*log(1:N + parms1)))
+#  tampon3 <- sum(-log(1:N + parms1)*exp(-parms2*log(1:N + parms1)))
+#  A <- sum((parms2/(x + parms1))) + (tampon/invC)
+#  B <- sum(log(x + parms1)) + (tampon3/tampon2)
+#  H <- matrix(1:4, nrow=2, byrow=TRUE)
+#  H[1,1] <- -sum(parms2/(x + parms1)^2)
+#  H[2,2] <- 0
+#  H[1,2] <- sum(1/(x + parms1))
+#  H[2,1] <- sum(1/(x + parms1))
+#  attr(res, "gradient") <- c(A, B)
+#  attr(res, "hessian") <- H
+#  res
+#}
+
+#On teste les fonctions
+negloglik(parms = c(2, 4))
 negloglik2(2, 4)
+negloglik3(parms = c(2, 4))
+negloglik4(2, 4)
 gradient(c(2, 4))
 
-#Essayons différentes méthodes d'optimisation
+#Essayons différentes méthodes d'optimisation avec mle
 #D'abord la méthode par défaut, Nelder-Mead, qui ne donne rien
-mle(negloglik6, start=list(parms1=2, parms2=4), method = "Nelder-Mead")
+mle(negloglik2, start=list(parms1=2, parms2=4), method = "Nelder-Mead")
 #Ensuite d'autres méthodes ne fonctionnant pas
-mle(negloglik6, start=list(parms1=2, parms2=4), method = "BFGS")
-mle(negloglik6, start=list(parms1=2, parms2=4), method = "CG")
-mle(negloglik6, start=list(parms1=2, parms2=4), method = "L-BFGS-B")
-ml <- mle(negloglik6, start=list(parms1=2, parms2=4), gr = gradient, method = "SANN")
+mle(negloglik2, start=list(parms1=2, parms2=4), method = "BFGS")
+mle(negloglik2, start=list(parms1=2, parms2=4), method = "CG")
+mle(negloglik2, start=list(parms1=2, parms2=4), method = "L-BFGS-B")
+mle(negloglik2, start=list(parms1=2, parms2=4), gr = gradient, method = "SANN")
 
 #mle(minuslogl = negloglik, par=list(parms1=2, parms2=4), gr = gradient, method = "BFGS")
 #optim(negloklik, start=list(parms1=2, parms2=4), gr=gradient)
 #nlm(function(p) negloglik(p), p=c(2, 4), gradtol=1e-9, steptol=1e-9)
 
-install.packages("ucminf")
-install.packages("numDeriv")
+#install.packages("ucminf")
+#install.packages("numDeriv")
 library(ucminf)
 library(numDeriv)
 
-#Let's use different geenral optimizers of R
+#Let's use different general optimizers of R
+#D'abord optim, où l'on peut préciser le gradient avec gr=...
+#d'abord sans préciser le gradient, puis ensuite en le précisant
 optim = optim(par = c(2, 4), fn = negloglik, hessian = F)
 optim2 = optim(par = c(2, 4), fn = negloglik, gr = gradient, hessian = F, method="BFGS")
-nlm = nlm(f = negloglik3, p = rep(2, 4), hessian = F)
-nlm2 = nlm(f = negloglik5, p = rep(2, 4), hessian = TRUE)
-nlminb = nlminb(start = rep(2, 4), objective = negloglik3)
-ucminf::ucminf(par=rep(2, 4), fn = negloglik, hessian = 1)
 optim
 optim2
+#Vérifions que le gradient est nul aux points optimaux
+gradient(optim$par)
+gradient(optim2$par) #Ce n'est pas le cas...
+
+#Ensuite nlm, où le gradient doit être précisé comme attribut de la fonction, on utilise donc negloglik3
+nlm <- nlm(negloglik, p=c(20,20), gradtol = 1e-12, steptol = 1e-9)
+nlm2 = nlm(f = negloglik3, p = rep(2, 4), hessian = F)
+#nlm3 = nlm(f = negloglik5, p = rep(2, 4), hessian = TRUE)
 nlm
 nlm2
-ml
-optimHess(par = rep(2, 4), fn = negloglik
+nlm3
+#On obtient un meilleur résultat avec la fonction sans gradient...
+gradient(nlm$estimate)
+gradient(nlm3$estimate) #Cela ne fonctionne pas...
 
+nlminb = nlminb(start = c(2, 4), objective = negloglik, gradient = gradient)
+ucminf::ucminf(par=rep(2, 4), fn = negloglik, hessian = 1)
+nlminb
 
+#optimHess(par = c(2, 4), fn = negloglik)
 
-
-
-
-
-#negloglik(c(1,1), x=2 )
-plot(d <- sapply(1:N, function(x) exp(-negloglik(5, 5))))  # here I changed 10 to N (the size of my data)
-x <- sample(1:N, 1000, replace = T, prob = d)
-ml <- nlm(function(p) negloglik(p, x=Genera_Sedum$Images_Sedum), p=c(20,20), gradtol = 1e-12, steptol = 1e-9)
-ml
